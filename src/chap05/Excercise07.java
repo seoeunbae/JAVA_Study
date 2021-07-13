bfdfd417dc45eb2aa1617496da5ccdace4c4be12
SeoeunBae <enyoupark@gmail.com>
2021-07-13 17:26:14 +0900

    JAVA_chap.03,04,05

--- src/chap05/Excercise07.java
+++ src/chap05/Excercise07.java
@@ -1,5 +1,13 @@
 package chap05;
 
 public class Excercise07 {
-
+    public static void main(String[] args) {
+        int max =0;
+        int[] array = { 1,5,3,8,2 };
+        max = array[0];
+        for(int i=0;i<=4;i++){
+            if(max<array[i]) max=array[i];
+        }
+        System.out.println(max);
+    }
 }
